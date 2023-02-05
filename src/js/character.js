cf5a2444df0931bd11b24bba77ec6d3ee8454c96
Antonio-87 <davydenko.87@list.ru>
2023-02-05 21:49:45 +0300

    revision

--- src/js/character.js
+++ src/js/character.js
@@ -1,18 +1,22 @@
 export default class Character {
     constructor(name, type) {
+        const HEROES = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
         if ((typeof (name) === 'string' && typeof (type) === 'string')
             && (name.length >= 2 && name.length <= 10)
-            && (type === 'Bowman' || type === 'Swordsman'
-            || type === 'Magician' || type === 'Daemon'
-            || type === 'Undead' || type === 'Zombie')) {
+            && HEROES.includes(type)) {
             this.name = name;
             this.type = type;
             this.health = 100;
             this.level = 1;
-        } else {
+        } else if (name instanceof TypeError) {
             throw new Error(
                 `
                 name - строка, min - 2 символа, max - 10\n
+                `,
+            );
+        } else if (type instanceof TypeError) {
+            throw new Error(
+                `
                 type - один из типов (строка): Bowman, Swordsman, Magician, Daemon, Undead, Zombie
                 `,
             );
